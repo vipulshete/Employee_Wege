3a3dd3ee74ddedc5d3e098b140ef0e347dde08bd
vipulshete <vipulshete724@gmail.com>
2022-07-16 18:19:28 +0530

    modifies : calculating month wege

--- EmployeeWege.java
+++ EmployeeWege.java
@@ -7,29 +7,34 @@ public class EmployeeWege {
 		int noOfHoursPerDay = 8;
 		int noOfHoursPerDayPartTime = 4;
 		int wegePerHours = 20;
+		int daysInMonth = 20;
+		
+		int attandance = 1;
+		double randomCheck = Math.floor(Math.random() * 10) % 2;
+		
 		// employee present or absent //	
-		int attendence= (int) (Math.floor(Math.random()*10))%2;
-	     
-		switch (attendence) {
-
-		case 1:
-			System.out.println("Employee is Working Full Time");
-			int dailyWage= wegePerHours * noOfHoursPerDay;
-			System.out.println("Employee daily wage is :" + dailyWage );
-		  break;
-		case 0:
-		    System.out.println("Employee is working part time");
-		    int dailPartTimeyWage= wegePerHours * noOfHoursPerDayPartTime ;
-		    System.out.println("Employee daily wage is "+ dailPartTimeyWage);
-		    break;
-
-		 default :
-		 System.out.println("Employee is absent");
-		    break;
+		if(attandance == randomCheck) {
+			
+			if(noOfHoursPerDay == 8) {
+				int totalWege = noOfHoursPerDay * wegePerHours;
+				System.out.println("Employee is present");
+				System.out.println("Wege per day is " + totalWege);
+			} 
+			if (noOfHoursPerDayPartTime == 4) {
+				int partTimeWege = noOfHoursPerDayPartTime * wegePerHours;
+				System.out.println("Employee is present part time");
+				System.out.println("Part time Wege is " + partTimeWege);
+			} 
+			if(daysInMonth == 20) {
+				int monthWege = noOfHoursPerDay * wegePerHours * daysInMonth;
+				System.out.println("Employee is present full time");
+				System.out.println("Wege of month is " + monthWege);
+			        // (noOfHoursPerDay * wedgePerHours);
+			}
+		} else {
+			System.out.println("Employee is Absent");
 		}
 		
-		
-
 	}
 
 }
@@ -37,20 +42,3 @@ public class EmployeeWege {
 
 
 
-//int attandance = 1;
-//double randomCheck = Math.floor(Math.random() * 10) % 2;
-//                         
-//if(attandance == randomCheck) {
-//	if(noOfHoursPerDay == 8) {
-//		int totalWege = noOfHoursPerDay * wegePerHours;
-//		System.out.println("Employee is present");
-//		System.out.println("Wege per day is " + totalWege);
-//	} 
-//	if (noOfHoursPerDayPartTime == 4) {
-//		int partTimeWege = noOfHoursPerDayPartTime * wegePerHours;
-//		System.out.println("Employee is present part time");
-//		System.out.println("Part time Wege is " + partTimeWege);
-//	} 
-//} else {
-//	System.out.println("Employee is Absent");
-//}

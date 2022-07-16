e242a60a5b1f22b2eb6233267f87dbdfea140a76
vipulshete <vipulshete724@gmail.com>
2022-07-16 17:55:31 +0530

    modified : employee daily wege

--- EmployeeWege.java
+++ EmployeeWege.java
@@ -4,16 +4,21 @@ public class EmployeeWege {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wege computation");
+		int noOfHoursPerDay = 8;
+		int wegePerHours = 20;
 		
 		// employee present or absent //			
-				int attandance = 1;
-				double randomCheck = Math.floor(Math.random() * 10) % 2;
-				//System.out.println(randomCheck);                         
-				if(attandance == randomCheck) {
-					System.out.println("Employee is present");
-				} else {
-					System.out.println("Employee is Absent");
-				}
+		
+		int attandance = 1;
+		double randomCheck = Math.floor(Math.random() * 10) % 2;
+		                         
+		if(attandance == randomCheck) {
+			System.out.println("Employee is present");
+			int totalWege = noOfHoursPerDay * wegePerHours;
+			System.out.println("Wege per day is " + totalWege);
+		} else {
+			System.out.println("Employee is Absent");
+		}
 
 	}
 

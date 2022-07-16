0b6a6e585c1f69572fb50c45ae7c8f8ca34173c2
vipulshete <vipulshete724@gmail.com>
2022-07-16 17:48:19 +0530

    employee is present or absent

--- EmployeeWege.java
+++ EmployeeWege.java
@@ -4,6 +4,17 @@ public class EmployeeWege {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wege computation");
+		
+		// employee present or absent //			
+				int attandance = 1;
+				double randomCheck = Math.floor(Math.random() * 10) % 2;
+				//System.out.println(randomCheck);                         
+				if(attandance == randomCheck) {
+					System.out.println("Employee is present");
+				} else {
+					System.out.println("Employee is Absent");
+				}
+
 	}
 
 }
